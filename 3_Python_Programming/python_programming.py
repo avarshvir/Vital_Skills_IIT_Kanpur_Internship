dce64e14231d7bb7ba5b03f1bd7ede306e0e4ee6
Arshvir <avarshvir@gmail.com>
2024-08-27 22:33:44 +0530

    commit

---
+++ 3_Python_Programming/python_programming.py
@@ -0,0 +1,18 @@
+"""
+I already have a dedicated python GitHub repository from scratch to advance level.
+Practical Topics Covered:
+1. Python Basics (Input/Output)
+2. Basic Programs
+3. List, Tuples, Sets, Dictionaries
+4. Conditionals Statements and Loops
+5. Functions
+6. OOPs
+7. File Handling and Error Handling
+8. Numpy, Pandas, MatplotLib
+9. OpenCV
+10. Python Graphics (Tkinter)
+11. Database in Python
+12. Networking in Python
+"""
+print("Hello to all!")
+print("https://github.com/avarshvir/Python-Learning-Journey")
