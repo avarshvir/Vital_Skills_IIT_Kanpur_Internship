2ee5c9eb0d4c0becce9e7908257d673246f6d7b0
Arshvir <avarshvir@gmail.com>
2024-08-31 16:58:11 +0530

    commit

---
+++ 4_Libraries_and_Data_Analysis_and_Preprocessing/4_2_Pandas/4_2_2_DataFrame.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data1 = {"A": [1, 2, 3, 4, 5],
+         "B": [6, 7, 8, 9, 10]}
+
+df = pd.DataFrame(data1, index=['a', 'b', 'c', 'd', 'e'])
+print(df)
+print("------------------------------------------")
+# Rename columns
+df.columns = ['A1', 'A2']
+
+print(df)
