7990a8b2747e85fb31d155bde146a9081bdc08a5
Arshvir <avarshvir@gmail.com>
2024-08-31 17:30:47 +0530

    commit

--- 4_Libraries_and_Data_Analysis_and_Preprocessing/4_2_Pandas/4_2_3_DataFrame.py
+++ 4_Libraries_and_Data_Analysis_and_Preprocessing/4_2_Pandas/4_2_3_DataFrame.py
@@ -8,3 +8,34 @@ df = pd.DataFrame(randn(5,4),index='A B C D E'.split(),columns='W X Y Z'.split()
 print(df)
 
 print("----------------------------------------------")
+
+#selection and indexing
+print(df[['W','Z']])
+print("-----------------")
+print(df['W'])
+print("--------------------------")
+#SQL syntax (not recommended)
+print(df.W)
+print("-------------------------------------")
+#creating new column
+df['new'] = df['X'] + df['Y']
+print(df)
+print("----------------------------------------")
+#removing columns
+# 0 -> for row, 1 -> coumns
+df.drop('new',axis=1,inplace=True)
+print(df)
+print("----------------------------------------")
+#removing row
+df.drop('D',axis=0,inplace=True)
+print(df)
+print("-------------------------------------------")
+# selecting row
+print(df.loc['A'])
+print("----------------------")
+#selecting
+print(df.iloc[0])
+print("------------------------------------------")
+#conditional selection
+print(df>0)  # boolean value
+print(df[df>0])     #numeric value
