33d0bb003e51a7c812b064990dc88212e24cd60d
Arshvir <avarshvir@gmail.com>
2024-08-31 17:02:54 +0530

    commit

--- 4_Libraries_and_Data_Analysis_and_Preprocessing/4_2_Pandas/4_2_2_DataFrame.py
+++ 4_Libraries_and_Data_Analysis_and_Preprocessing/4_2_Pandas/4_2_2_DataFrame.py
@@ -3,7 +3,7 @@ import pandas as pd
 data1 = {"A": [1, 2, 3, 4, 5],
          "B": [6, 7, 8, 9, 10]}
 
-df = pd.DataFrame(data1, index=['a', 'b', 'c', 'd', 'e'])
+df = pd.DataFrame(data1, index='a b c d e'.split())
 print(df)
 print("------------------------------------------")
 # Rename columns
