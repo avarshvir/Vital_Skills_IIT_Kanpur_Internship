b0d3090e930e34053fe92d4836553a1403229133
Arshvir <avarshvir@gmail.com>
2024-08-31 17:50:22 +0530

    commit

---
+++ 4_Libraries_and_Data_Analysis_and_Preprocessing/4_2_Pandas/4_2_4_Missing_Data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+df = pd.DataFrame({
+    'A': [1,2,np.nan],
+    'B': [5,np.nan,np.nan],
+    'C':[1,2,3]
+},index=['a1', 'a2', 'a3'])
+
+print(df)
+print("----------------------")
+print(df.dropna())
+print("----------------------")
+print(df)
+
+print("-----------------------------")
+#fill values
+print(df.fillna('Fill'))
+print(df.fillna(value = df.mean(),inplace=True))
+print(df)
