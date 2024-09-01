ac5923e4ad630af6aca1340bfe62b784d215b5f0
Arshvir <avarshvir@gmail.com>
2024-09-01 20:03:18 +0530

    commit

---
+++ 4_Libraries_and_Data_Analysis_and_Preprocessing/4_3_Matplotlib/4_3_2_multiple_plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+X = np.array([1,2,3,4,5,6])
+y = np.array([1.2,0.2,1,5,2,7])
+
+plt.subplot(1,2,1)
+plt.plot(X,y)
+plt.subplot(1,2,2)
+plt.plot(X,y)
+plt.show()
